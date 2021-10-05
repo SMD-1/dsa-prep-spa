d683638e3161e87ec3677b50a7342291d67c6da6
Danish <skdanish1999@gmail.com>
2021-10-05 13:15:59 +0530

    Attempt to add splitter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 // import Button from "@mui/material/Button";
-import CustomEditor from "./components/CustomEditor";
+// import CustomEditor from "./components/CustomEditor";
 import NavBar from "./components/NavBar";
-import { Grid } from "@mui/material";
-import LeftText from "./components/LeftText";
+// import { Grid } from "@mui/material";
+// import LeftText from "./components/LeftText";
+import Splitter from "./components/splitter/Splitter";
 // import { makeStyles } from "@mui/styles";
 
 // const useStyles = makeStyles({});
@@ -11,15 +12,17 @@ function App() {
   return (
     <div>
       <NavBar />
+      <Splitter />
       {/* <Button variant="contained">Hello World</Button>; */}
-      <Grid container spacing={2} justifyContent="space-between">
+
+      {/* <Grid container spacing={2} justifyContent="space-between">
         <Grid item xs={6}>
           <LeftText />
         </Grid>
         <Grid item xs={6}>
           <CustomEditor />
         </Grid>
-      </Grid>
+      </Grid> */}
     </div>
   );
 }
